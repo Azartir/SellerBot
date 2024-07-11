3354d7dba25c647ef3d3542f4d3e13a89c2af358
azartir <azartir@yandex.ru>
2024-07-11 13:06:08 +0300

    final to realise

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 # Укажите ваш токен и ID администратора здесь
 TELEGRAM_BOT_TOKEN = "7469802414:AAGeN86RbNCEChX4eZ5mOkLa2wn2kew73L0"
-ADMIN_ID = "YOUR_ADMIN_ID"
+ADMIN_ID = "6459838560"
 
 # Инициализация бота
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
@@ -24,90 +24,183 @@ def load_data():
             with open(data_file, 'r') as f:
                 return json.load(f)
         except json.JSONDecodeError:
+            logger.error("Ошибка при загрузке данных из JSON файла")
             return {}
     else:
         return {}
 
 # Сохранение данных в файл
 def save_data(data):
-    with open(data_file, 'w') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-
-# Инициализация базы данных из JSON
-user_data = load_data()
+    try:
+        with open(data_file, 'w') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+    except Exception as e:
+        logger.error(f"Ошибка при сохранении данных в JSON файл: {str(e)}")
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    user_id = str(message.from_user.id)
-    username = message.from_user.username
-    if user_id not in user_data:
-        user_data[user_id] = {'username': username, 'balance': 0}
-        save_data(user_data)
-    
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.add(telebot.types.InlineKeyboardButton("Купить UC", callback_data='buy_uc'))
-    keyboard.add(telebot.types.InlineKeyboardButton("Баланс", callback_data='balance'))
-    bot.send_message(message.chat.id, 'Привет! Выберите опцию:', reply_markup=keyboard)
+    try:
+        user_id = str(message.from_user.id)
+        username = message.from_user.username
+        user_data = load_data()
+        if user_id not in user_data:
+            user_data[user_id] = {'username': username, 'balance': 0}
+            save_data(user_data)
+        
+        keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        keyboard.row(telebot.types.KeyboardButton("Купить UC"))
+        keyboard.row(telebot.types.KeyboardButton("Баланс"))
+        bot.send_message(message.chat.id, 'Привет! Выберите опцию:', reply_markup=keyboard)
+        logger.info(f"Пользователь {username} ({user_id}) запустил бота")
+    except Exception as e:
+        logger.error(f"Ошибка при обработке команды /start: {str(e)}")
+
+# Функция для генерации клавиатуры с выбором прайса UC
+def generate_price_keyboard():
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard.row(telebot.types.KeyboardButton("60UC - 85 р"))
+    keyboard.row(telebot.types.KeyboardButton("325UC - 415р"))
+    keyboard.row(telebot.types.KeyboardButton("355UC - 450р"))
+    keyboard.row(telebot.types.KeyboardButton("660UC - 810р"))
+    keyboard.row(telebot.types.KeyboardButton("720UC - 855р"))
+    keyboard.row(telebot.types.KeyboardButton("1075UC - 1300р"))
+    keyboard.row(telebot.types.KeyboardButton("1800UC - 2050р"))
+    keyboard.row(telebot.types.KeyboardButton("1950UC - 2200р"))
+    keyboard.row(telebot.types.KeyboardButton("3850UC - 4050р"))
+    keyboard.row(telebot.types.KeyboardButton("4000UC - 4300р"))
+    keyboard.row(telebot.types.KeyboardButton("8100UC - 8200р"))
+    keyboard.row(telebot.types.KeyboardButton("Назад"))
+    return keyboard
 
 # Обработчик нажатий на кнопки
-@bot.callback_query_handler(func=lambda call: True)
-def handle_buttons(call):
+@bot.message_handler(func=lambda message: True)
+def handle_buttons(message):
     try:
-        user_id = str(call.from_user.id)
-        if call.data == 'buy_uc':
-            keyboard = telebot.types.InlineKeyboardMarkup()
-            keyboard.row(telebot.types.InlineKeyboardButton("60UC - 85 р", callback_data='60UC_85'))
-            keyboard.row(telebot.types.InlineKeyboardButton("325UC - 415р", callback_data='325UC_415'))
-            keyboard.row(telebot.types.InlineKeyboardButton("355UC - 450р", callback_data='355UC_450'))
-            keyboard.row(telebot.types.InlineKeyboardButton("660UC - 810р", callback_data='660UC_810'))
-            keyboard.row(telebot.types.InlineKeyboardButton("720UC - 855р", callback_data='720UC_855'))
-            keyboard.row(telebot.types.InlineKeyboardButton("1075UC - 1300р", callback_data='1075UC_1300'))
-            keyboard.row(telebot.types.InlineKeyboardButton("1800UC - 2050р", callback_data='1800UC_2050'))
-            keyboard.row(telebot.types.InlineKeyboardButton("1950UC - 2200р", callback_data='1950UC_2200'))
-            keyboard.row(telebot.types.InlineKeyboardButton("3850UC - 4050р", callback_data='3850UC_4050'))
-            keyboard.row(telebot.types.InlineKeyboardButton("4000UC - 4300р", callback_data='4000UC_4300'))
-            keyboard.row(telebot.types.InlineKeyboardButton("8100UC - 8200р", callback_data='8100UC_8200'))
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                  text="Выберите количество UC:", reply_markup=keyboard)
+        user_id = str(message.from_user.id)
+        user_data = load_data()
+        if message.text == 'Купить UC':
+            keyboard = generate_price_keyboard()
+            bot.send_message(message.chat.id, "Выберите количество UC:", reply_markup=keyboard)
+            logger.info(f"Пользователь {user_data[user_id]['username']} ({user_id}) выбрал покупку UC")
         
-        elif call.data.endswith('UC'):
-            uc_amount, price = call.data.split('_')
-            price = int(price)
+        elif any(uc in message.text for uc in ['UC']):
+            uc_amount, price = message.text.split(' - ')
+            uc_amount = uc_amount.replace('UC', '').strip()
+            price = int(price.replace('р', '').strip())
             balance = user_data[user_id]['balance']
 
             if balance >= price:
-                bot.send_message(call.message.chat.id, f"Вы выбрали {uc_amount}. Отправьте ваш ID в PUBG.")
-                bot.register_next_step_handler(call.message, process_uc_purchase, uc_amount, price)
+                bot.send_message(message.chat.id, f"Вы выбрали {uc_amount} UC. Отправьте ваш ID в PUBG.")
+                bot.register_next_step_handler(message, process_uc_purchase, uc_amount, price)
+                logger.info(f"Пользователь {user_data[user_id]['username']} ({user_id}) выбрал {uc_amount} за {price} р.")
             else:
-                bot.send_message(call.message.chat.id, "У вас недостаточно средств для этой покупки.")
+                bot.send_message(message.chat.id, "У вас недостаточно средств для этой покупки.")
+                logger.warning(f"Пользователь {user_data[user_id]['username']} ({user_id}) попытался купить {uc_amount}, но недостаточно средств")
 
-        elif call.data == 'balance':
+        elif message.text == 'Баланс':
             balance = user_data[user_id]['balance']
-            keyboard = telebot.types.InlineKeyboardMarkup()
-            keyboard.add(telebot.types.InlineKeyboardButton("Пополнить", callback_data='top_up'))
-            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                  text=f"Ваш баланс: {balance} UC", reply_markup=keyboard)
+            keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+            keyboard.row(telebot.types.KeyboardButton("Пополнить"))
+            keyboard.row(telebot.types.KeyboardButton("Назад"))
+            bot.send_message(message.chat.id, f"Ваш баланс: {balance} UC", reply_markup=keyboard)
+            logger.info(f"Пользователь {user_data[user_id]['username']} ({user_id}) запросил баланс")
 
-        elif call.data == 'top_up':
-            bot.send_message(call.message.chat.id, "Совершите перевод на указанную вами сумму Карта: 2200700495101577 В ОТВЕТНОМ СООБЩЕНИИ ПРИШЛИТЕ ЧЕК ТРАНЗАКЦИИ:")
+        elif message.text == 'Пополнить':
+            bot.send_message(message.chat.id, "Совершите перевод на указанную вами сумму на карту 2200700495101577 и отправьте фото чека.")
+            bot.register_next_step_handler(message, handle_top_up_request)
+            logger.info(f"Пользователь {user_data[user_id]['username']} ({user_id}) запросил пополнение")
+            
+        elif message.text == 'Назад':
+            handle_start(message)
+            logger.info(f"Пользователь {user_data[user_id]['username']} ({user_id}) вернулся на стартовый экран")
 
     except Exception as e:
         logger.error(f"Ошибка при обработке нажатия кнопки: {str(e)}")
 
-# Обработчик текстовых сообщений (комментариев или запросов)
+# Обработчик запроса на пополнение баланса
+def handle_top_up_request(message):
+    try:
+        user_id = str(message.from_user.id)
+        user_data = load_data()
+        if message.content_type == 'photo':
+            # Обработка фото
+            bot.send_message(message.chat.id, "Спасибо! Ваш чек получен и будет проверен администратором.")
+            logger.info(f"Пользователь {user_data[user_id]['username']} ({user_id}) отправил чек пополнения баланса")
+
+            # Отправка чека администратору с просьбой подтвердить
+            markup = telebot.types.InlineKeyboardMarkup()
+            real_check_button = telebot.types.InlineKeyboardButton(text='Реальный чек', callback_data=f'real_check_{user_id}')
+            invalid_check_button = telebot.types.InlineKeyboardButton(text='Некорректный чек', callback_data=f'invalid_check_{user_id}')
+            markup.add(real_check_button, invalid_check_button)
+            
+            bot.forward_message(ADMIN_ID, message.chat.id, message.message_id)
+            bot.send_message(ADMIN_ID, f"Пользователь {user_data[user_id]['username']} прислал чек. Выберите действие:", reply_markup=markup)
+            
+        else:
+            bot.send_message(message.chat.id, "Пожалуйста, отправьте чек пополнения в формате фото.")
+            bot.register_next_step_handler(message, handle_top_up_request)
+            logger.warning(f"Пользователь {user_data[user_id]['username']} ({user_id}) отправил неподдерживаемый тип данных для чека")
+    except Exception as e:
+        logger.error(f"Ошибка при обработке запроса на пополнение баланса: {str(e)}")
+        bot.send_message(message.chat.id, "Произошла ошибка при обработке вашего чека. Попробуйте еще раз позже.")
+
+# Обработчик процесса покупки UC
 def process_uc_purchase(message, uc_amount, price):
     try:
         user_id = str(message.from_user.id)
-        bot.send_message(ADMIN_ID, f"Пользователь {user_data[user_id]['username']} хочет пополнить на {price} руб. ({uc_amount}). Сообщение: {message.text}")
-        bot.send_message(message.chat.id, f"Ваш запрос на {uc_amount} отправлен администратору.")
+        pubg_id = message.text.strip()
+        user_data = load_data()
         
-        # Обновление баланса
-        user_data[user_id]['balance'] -= price
-        save_data(user_data)
-
+        if pubg_id.isdigit():
+            user_data[user_id]['balance'] -= price
+            save_data(user_data)
+            bot.send_message(message.chat.id, f"Ваш заказ на {uc_amount} UC принят. ID в PUBG: {pubg_id}. Баланс обновлен, ожидайте пополнения.")
+            bot.send_message(ADMIN_ID, f"Пользователь {user_data[user_id]['username']} ({user_id}) купил {uc_amount} UC. ID в PUBG:")
+            bot.send_message(ADMIN_ID, f"{pubg_id}")
+            logger.info(f"Пользователь {user_data[user_id]['username']} ({user_id}) купил {uc_amount} UC. ID в PUBG: {pubg_id}.")
+        else:
+            bot.send_message(message.chat.id, "Введите корректный ID в PUBG.")
+            bot.register_next_step_handler(message, process_uc_purchase, uc_amount, price)
+            
     except Exception as e:
         logger.error(f"Ошибка при обработке покупки UC: {str(e)}")
+        bot.send_message(message.chat.id, "Произошла ошибка при обработке вашего заказа. Попробуйте еще раз позже.")
+
+# Обработчик нажатий на кнопки "реальный чек" и "некорректный чек" у администратора
+@bot.callback_query_handler(func=lambda call: True)
+def handle_admin_buttons(call):
+    try:
+        user_id = call.data.split('_')[-1]
+        user_data = load_data()
+        if call.data.startswith('real_check'):
+            bot.send_message(call.message.chat.id, f"Введите сумму для пополнения баланса пользователя {user_data[user_id]['username']}:")
+            bot.register_next_step_handler(call.message, lambda message: confirm_top_up(message, user_id))
+        
+        elif call.data.startswith('invalid_check'):
+            bot.send_message(user_id, "Ваш чек не прошел проверку. Пожалуйста, отправьте корректный чек для пополнения баланса.")
+    except Exception as e:
+        logger.error(f"Ошибка при обработке нажатия кнопки администратором: {str(e)}")
+
+# Функция для подтверждения пополнения баланса администратором
+def confirm_top_up(message, user_id):
+    try:
+        admin_message = message.text.strip()
+        user_data = load_data()
+        if admin_message.isdigit():
+            amount = int(admin_message)
+            user_data[user_id]['balance'] += amount
+            save_data(user_data)
+            bot.send_message(ADMIN_ID, f"Баланс пользователя {user_data[user_id]['username']} успешно пополнен на {amount} UC.")
+            bot.send_message(user_id, f"Ваш баланс успешно пополнен на {amount} UC.")
+            logger.info(f"Пополнение баланса пользователя {user_data[user_id]['username']} ({user_id}) на {amount} UC подтверждено.")
+        else:
+            bot.send_message(ADMIN_ID, "Введите корректное число.")
+            # Регистрация следующего шага для администратора в случае некорректного ввода
+            bot.register_next_step_handler_by_chat_id(ADMIN_ID, lambda msg: confirm_top_up(msg, user_id))
+    except Exception as e:
+        logger.error(f"Ошибка при подтверждении пополнения баланса: {str(e)}")
+        bot.send_message(ADMIN_ID, "Произошла ошибка при подтверждении пополнения баланса. Попробуйте еще раз позже.")
 
 # Главная функция для запуска бота
 def main():
